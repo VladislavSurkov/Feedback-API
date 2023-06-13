ca0ce8aa9f0d330868121f1537c8a5089101ac77
VladislavSurkov <fvhabjrega@gmail.com>
2023-06-13 18:04:06 +0300

    message

--- controller/auth/registerUser.js
+++ controller/auth/registerUser.js
@@ -5,7 +5,7 @@ const registerUser = async (req, res, next) => {
   const { name, username, email, password } = req.body;
   const user = await findUserBy({ email });
 
-  if (user) throw new ConflictError({ message: "Email in use" });
+  if (user) throw new ConflictError( "Email in use" );
 
   const inRegUser = await regUser({ name,username, email, password });
 
