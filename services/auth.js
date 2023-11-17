c217417338f3ad5fb6995a6b722da368a62c2964
VladislavSurkov <fvhabjrega@gmail.com>
2023-11-17 22:59:34 +0200

    Update auth.js

--- services/auth.js
+++ services/auth.js
@@ -19,7 +19,7 @@ const createToken = ({ _id }) => {
     id: _id,
   };
 
-  return jwt.sign(playload, JWT_SECRET_KEY, { expiresIn: "1h" });
+  return jwt.sign(playload, JWT_SECRET_KEY, { expiresIn: "1m" });
 };
 
 const verifyToken = async (token) => {
